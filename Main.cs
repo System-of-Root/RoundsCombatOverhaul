b32c9d67b9fb4584ff651aad21ef9a9e9495f6bc
System-of-Root <root@r00t.systems>
2024-04-06 16:06:34 -0400

    Project setup

---
+++ Main.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+
+namespace RCO {
+    [BepInDependency("com.willis.rounds.unbound")]
+
+    [BepInProcess("Rounds.exe")]
+
+    [BepInPlugin(ModId, ModName, Version)]
+    public class Main:BaseUnityPlugin {
+        public const string ModId = "", 
+            ModName = "", 
+            Version = "0.0.0";
+
+        void Awake() {
+
+        }
+
+        void Start() {
+
+        }
+    }
+}
